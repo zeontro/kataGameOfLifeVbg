6d5113434188ec0f79e91f6864a141caeac27679
vincent bg <vincent.boily-grant.1@ulaval.ca>
2017-10-04 13:55:11 -0400

    d

---
+++ gameOfLife/src/main/java/katas/gameOfLife/App.java
@@ -0,0 +1,13 @@
+package katas.gameOfLife;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
